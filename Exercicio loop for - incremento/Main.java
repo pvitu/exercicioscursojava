395dbad087801408d31e8bdeca2a2e07e3023d3b
Paulo Araújo <128916254+pvitu@users.noreply.github.com>
2025-02-17 09:53:20 -0300

    Exercicio loop for

---
+++ Exercicio loop for - incremento/Main.java
@@ -0,0 +1,24 @@
+/*
+Fazer um programa que lê um valor inteiro N e depois N números inteiros.
+
+Ao final, mostrar a soma dos N números lidos.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite quantos números você deseja somar, depois digite os números: ");
+        int N = sc.nextInt();
+        int soma = 0;
+        for (int i=0; i<N; i++){
+            int x = sc.nextInt();
+            soma = soma + x;
+        }
+
+        System.out.println("A soma dos números é: " + soma);
+        sc.close();
+    }
+}
